769f77a9eff68635fee3f0a9715a0fef0b9daee2
oleg-usatyi <oleg_usatyi@ukr.net>
2024-02-24 23:38:38 +0100

    added the ability to stop code execution

--- task_1.py
+++ task_1.py
@@ -3,18 +3,27 @@ import time
 
 queue = Queue()
 
-def generate_request():
-    req = "Request " + str(time.time())
+def generate_request(request_id):
+    req = f"Request {request_id}"
+    print(f"Generated {req}")
     queue.put(req)
-
+    time.sleep(1)
 
 def process_request():
     if not queue.empty():
         req = queue.get()
-        print(f"Serve request {req}")
+        print(f"Processed {req}")
+        time.sleep(2)
     else:
         print('Queue is empty')
 
+request_id = 1
 while True:
-    generate_request()
+    generate_request(request_id)
     process_request()
+    request_id += 1
+
+    user_input = input("Continue? (y/n): ")
+    if user_input.lower() != 'y':
+        print("Program terminated.")
+        break
